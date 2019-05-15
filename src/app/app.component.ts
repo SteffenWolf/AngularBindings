1e7c2c1cb75e6368d9a63b2ea08c70bc973c7e24
Stefen Allen <Stefen.Allen@gmail.com>
2019-05-15 15:15:12 -0600

    Used the different types of data bindings as well as interpolations to build a simple app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  username = '';
   title = 'data-binding-assignment';
+  buttonStatus = true;
+
+  onUsernameChange() {
+    if(this.username === ''){
+      this.buttonStatus = true;
+    } else {
+      this.buttonStatus = false;
+    }
+  }
+
+  reset() {
+    this.username = '';
+    this.onUsernameChange();
+  }
+
 }
